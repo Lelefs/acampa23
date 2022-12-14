a4291e0e39ebb9476c23b54c99d7527ddcc1427e
Leandro Finochio <lefinochio@hotmail.com>
2022-12-14 17:47:56 -0300

    teste pwa

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'Acampa MAG SBC',
+    shortName: 'Acampa',
+  },
 });
