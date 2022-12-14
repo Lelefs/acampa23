5ffe2ee43cffa9ffba4bf90311217a4c20b8488d
Leandro Finochio <lefinochio@hotmail.com>
2022-12-14 20:24:01 -0300

    Imagens

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    name: 'Acampa MAG SBC',
-    shortName: 'Acampa',
+    name: 'MAG SBC - Acampa 2023',
+    short_name: 'A23 - EU SOU',
+    theme_color: '#000',
+    background_color: '#000',
   },
 });
