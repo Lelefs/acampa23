885b2151643f643c41ae837dc7aa95a274775d43
Leandro Finochio <lefinochio@hotmail.com>
2022-12-22 23:13:30 -0300

    Modal rifa

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,6 +37,38 @@
 
       <MidiasComponent />
     </main>
+
+    <v-dialog v-model="dialog" transition="dialog-bottom-transition" max-width="900" persistent>
+      <v-card>
+        <v-card-title>
+          <v-btn class="ml-auto" icon color="#111827" @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-card-text>
+          <v-img width="100%" src="img/amigos-proposito.jpeg" alt="Amigos do propósito" class="rounded"></v-img>
+
+          <h1 class="title-modal-rifa">AMIGOS DO PROPÓSITO</h1>
+        </v-card-text>
+
+        <v-card-actions class="justify-center pb-5">
+          <v-btn class="px-5 font-weight-bold" outlined rounded color="#6E0073" @click="dialog = false"> FECHAR </v-btn>
+
+          <v-btn
+            class="px-5 font-weight-bold"
+            rounded
+            color="#6E0073"
+            href="https://rifa.digital/s/amigos-do-proposito-a23"
+            target="_blank"
+            @click="dialog = false"
+            style="color: #fff"
+          >
+            PARTICIPAR
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -49,6 +81,7 @@ export default {
 
   data() {
     return {
+      dialog: true,
       windowWidth: window.innerWidth,
     };
   },
@@ -114,6 +147,15 @@ export default {
   }
 }
 
+.title-modal-rifa {
+  font-weight: 400;
+  line-height: 105%;
+  text-align: center;
+  letter-spacing: 0.15em;
+  color: #170d3f;
+  margin: 24px 0 0;
+}
+
 @media only screen and (max-width: 720px) {
   .home-acamp {
     .title-page {
@@ -140,6 +182,10 @@ export default {
         text-align: center;
       }
     }
+  }
+
+  .title-modal-rifa {
+    font-size: 12px;
   }
 }
 </style>
