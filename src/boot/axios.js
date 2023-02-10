d9bc879646eb64fa34df41d02c2cd27bee50e6a1
Leandro Finochio <lefinochio@hotmail.com>
2023-02-09 23:33:09 -0300

    remover get env

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,15 +1,12 @@
 /* eslint-disable camelcase */
 import Vue from 'vue';
 import Axios from 'axios';
-import getEnv from '../utils/env';
-
-const BASE_URL = getEnv('VUE_APP_API_URL');
 
 const axios = Axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'https://api-cmn-pagamentos-evento.herokuapp.com/api',
 });
 
 Vue.prototype.$axios = axios;
 
-export const baseUrl = BASE_URL;
+export const baseUrl = 'https://api-cmn-pagamentos-evento.herokuapp.com/api';
 export default axios;
