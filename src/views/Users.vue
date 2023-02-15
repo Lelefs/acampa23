d83da27e729c38d215508e5aed23349a176a2d79
Leandro Finochio <lefinochio@hotmail.com>
2023-02-14 21:36:27 -0300

    Lista ônibus

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-card>
+  <v-card max-width="650" class="mx-auto">
     <v-card-title>
       Acampantes
       <v-spacer></v-spacer>
       <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
     </v-card-title>
-    <v-data-table :headers="headers" :items="usersFormatted" :search="search" disable-pagination hide-default-footer></v-data-table>
+    <v-data-table
+      :headers="headers"
+      :items="usersFormatted"
+      :search="search"
+      disable-pagination
+      hide-default-footer
+      sort-by="nome"
+    ></v-data-table>
   </v-card>
 </template>
 
@@ -18,8 +25,6 @@ export default {
   data: () => ({
     headers: [
       { text: 'Nome', align: 'start', value: 'nome' },
-      { text: 'Documento', align: 'start', value: 'documento' },
-      { text: 'Célula', align: 'start', value: 'nomeCelula' },
       { text: 'Quarto', align: 'start', value: 'quarto' },
       { text: 'Ônibus', align: 'start', value: 'onibus' },
     ],
